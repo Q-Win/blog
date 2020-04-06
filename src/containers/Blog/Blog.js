08f751f717905e1fee5c90bf305a3b9abbb2ef3f
Quinn Krug <quinn.krug@gmail.com>
2020-04-06 16:12:46 -0600

    make posts selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   }
 
   componentDidMount (){
@@ -25,9 +26,18 @@ class Blog extends Component {
       });
   }
 
+    postSelectionHandler = (id) => {
+      this.setState({selectedPostId: id})
+    }
+
     render () {
       const posts = this.state.posts.map(post =>{
-        return <Post key={post.id} title={post.title} author={post.author}/>
+        return <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectionHandler(post.id)}
+          />
       });
 
       return (
@@ -36,7 +46,7 @@ class Blog extends Component {
                 {posts}
               </section>
               <section>
-                  <FullPost />
+                  <FullPost id={this.state.selctedPostId} />
               </section>
               <section>
                   <NewPost />
