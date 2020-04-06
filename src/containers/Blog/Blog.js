0018a51da64f4da39e615e01529ace1887c76785
Quinn Krug <quinn.krug@gmail.com>
2020-04-06 16:29:16 -0600

    fetch data on update without creating infinite loop

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,53 +7,53 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null
-  }
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
 
-  componentDidMount (){
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => {
-        const posts = response.data.slice(0,4);
-        const updatedPosts =  posts.map(post=> {
-          return{
-            ...post,
-            author: 'Max'
-          }
-        })
-        this.setState({posts: updatedPosts});
-      });
-  }
+    componentDidMount () {
+        axios.get( 'https://jsonplaceholder.typicode.com/posts' )
+            .then( response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+                // console.log( response );
+            } );
+    }
 
-    postSelectionHandler = (id) => {
-      this.setState({selectedPostId: id})
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
     }
 
     render () {
-      const posts = this.state.posts.map(post =>{
-        return <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectionHandler(post.id)}
-          />
-      });
+        const posts = this.state.posts.map(post => {
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)} />;
+        });
 
-      return (
-          <div>
-              <section className="Posts">
-                {posts}
-              </section>
-              <section>
-                  <FullPost id={this.state.selctedPostId} />
-              </section>
-              <section>
-                  <NewPost />
-              </section>
-          </div>
-      );
-  }
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostId} />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
 }
 
 export default Blog;
